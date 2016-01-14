389dbfa167535c03049538a520aebc7760bf06a0
lethalgit <saurabhz@outlook.com>
2016-01-15 00:58:16 +0530

    Added posts controller and created 4 of it's methods

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'This is some sample text for our awesome new ruby blog.';
+		@content_second = 'This is more sample text for our awesome new ruby blog.'
+	end
+
+	def show
+
+	end
+
+	def create
+
+	end
+
+	def new
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+end
