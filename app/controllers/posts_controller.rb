88231cbb3e71ed2c7867407e3c0ef4cc59f757ce
lethalgit <saurabhz@outlook.com>
2016-01-15 18:43:26 +0530

    Made almost all the backend, working properly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,31 +4,44 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
-	end
-
-	def create
-
-	end
-
-	def new
-
+		@post = Post.find(params[:id]);
 	end
 
 	def edit
+		@post = Post.find(params[:id]);
+	end
 
+	def new
+		@post = Post.new;
+		@category = Category.all;
+	end
+
+	def create
+		@post = Post.new(post_params);
+		if @post.save
+			redirect_to @post, :notice => "New post has been created"
+		else
+			render "new"
+		end
 	end
 
 	def update
-
+		@post = Post.find(params[:id]);
+		if @post.update_attributes(post_params)
+			redirect_to post_path, :notice => "Your post has been updated"
+		else
+			render "edit"
+		end
 	end
 
 	def destroy
-
+		@post = Post.find(params[:id]);
+		@post.destroy
+		redirect_to posts_path, :notice => "Your post has been deleted."
 	end
 
-	# private
-	# def post_params
-	# 	params.require(:posts).permit(:title, :body, :category_id, :author_id);
-	# end
+	private
+	def post_params
+		params.require(:post).permit(:id, :title, :body, :category_id, :author_id);
+	end
 end
